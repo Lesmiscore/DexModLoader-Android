f846b2d14d802e31575e3716416018e245a19bf1
nao20010128nao <nao20010128@gmail.com>
2015-06-19 22:22:51 +0900

    JavaScript部分を作成(最初)

---
+++ jsSrc/main.js
@@ -0,0 +1,93 @@
+/* Java Mods Loader for BlockLauncher(Android)
+ * Created by nao20010128nao.
+ * DO NOT REDISTRIBUTE!
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+//Code Loader
+var mainDex = "";
+var dxTool = "";
+var mainManager, dxClass;
+
+
+
+//Hooks Observer
+function useItem(x, y, z, itemId, blockId, side, itemDamage, blockDamage) {
+
+}
+
+function destroyBlock(x, y, z, side) {
+
+}
+
+function startDestroyBlock(x, y, z, side) {
+
+}
+
+function newLevel(hasLevel) {
+
+}
+
+function selectLevelHook() {
+
+}
+
+function leaveGame() {
+
+}
+
+function attackHook(attacker, victim) {
+
+}
+
+function modTick() {
+
+}
+
+function procCmd(command) {
+
+}
+
+function deathHook(murderer, victim) {
+
+}
+
+function entityRemovedHook(entity) {
+
+}
+
+function entityAddedHook(entity) {
+
+}
+
+function levelEventHook(entity, eventType, x, y, z, data) {
+
+}
+
+function blockEventHook(x, y, z, eventType, data) {
+
+}
+
+function serverMessageReceiveHook(message) {
+
+}
+
+function chatReceiveHook(sender, message) {
+
+}
