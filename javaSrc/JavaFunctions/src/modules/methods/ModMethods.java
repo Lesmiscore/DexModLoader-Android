1d93f07a2f9848a894f213a6445c7f2117621b70
nao20010128nao <nao20010128@gmail.com>
2015-06-21 16:04:36 +0900

    ModPEオブジェクト再現開始&サイズ縮小

--- javaSrc/JavaFunctions/src/modules/methods/ModMethods.java
+++ javaSrc/JavaFunctions/src/modules/methods/ModMethods.java
@@ -4,112 +4,174 @@ import modules.MainManager;
 
 public final class ModMethods {
 	MainManager mm;
+	public ModPEObject ModPE = new ModPEObject();
 
 	public ModMethods(MainManager mm) {
 		this.mm = mm;
 	}
 
 	public void addItemInventory(int id, int amount, int damage) {
-		MainManager.callMethod("addItemInventory", new Object[] { id, amount,
-				damage });
+		mm.callMethod1("addItemInventory", new Object[] { id, amount, damage });
 	}
 
 	public void clientMessgae(String text) {
-		MainManager.callMethod("clientMessgae", new Object[] { text });
+		mm.callMethod1("clientMessgae", new Object[] { text });
 	}
 
 	public String getLevel() {
-		return MainManager.callMethod("getLevel", new Object[] {}).toString();
+		return mm.callMethod1("getLevel", new Object[] {}).toString();
 	}
 
 	public void getPitch(double pitch) {
-		MainManager.callMethod("getPitch", new Object[] { pitch });
+		mm.callMethod1("getPitch", new Object[] { pitch });
 	}
 
 	public Object getPlayerEnt() {
-		return MainManager.callMethod("getPlayerEnt", new Object[] {});
+		return mm.callMethod1("getPlayerEnt", new Object[] {});
 	}
 
 	public double getPlayerX() {
-		return (double) MainManager.callMethod("getPlayerX", new Object[] {});
+		return (double) mm.callMethod1("getPlayerX", new Object[] {});
 	}
 
 	public double getPlayerY() {
-		return (double) MainManager.callMethod("getPlayerY", new Object[] {});
+		return (double) mm.callMethod1("getPlayerY", new Object[] {});
 	}
 
 	public double getPlayerZ() {
-		return (double) MainManager.callMethod("getPlayerZ", new Object[] {});
+		return (double) mm.callMethod1("getPlayerZ", new Object[] {});
 	}
 
 	public int getTile(int x, int y, int z) {
-		return (int) MainManager
-				.callMethod("getTile", new Object[] { x, y, z });
+		return (int) mm.callMethod1("getTile", new Object[] { x, y, z });
 	}
 
 	public int getYaw(int direction) {
-		return (int) MainManager.callMethod("getYaw",
-				new Object[] { direction });
+		return (int) mm.callMethod1("getYaw", new Object[] { direction });
 	}
 
 	public void preventDefault() {
-		MainManager.callMethod("preventDefault", new Object[] {});
+		mm.callMethod1("preventDefault", new Object[] {});
 	}
 
 	public void print(String text) {
-		MainManager.callMethod("print", new Object[] { text });
+		mm.callMethod1("print", new Object[] { text });
 	}
 
 	public void rideAnimal(Object ridable, Object rider) {
-		MainManager.callMethod("rideAnimal", new Object[] { ridable, rider });
+		mm.callMethod1("rideAnimal", new Object[] { ridable, rider });
 	}
 
 	public void setNightMode(boolean nightMode) {
-		MainManager.callMethod("setNightMode", new Object[] { nightMode });
+		mm.callMethod1("setNightMode", new Object[] { nightMode });
 	}
 
 	public void setPosition(Object entity, double x, double y, double z) {
-		MainManager.callMethod("setPosition", new Object[] { entity, x, y, z });
+		mm.callMethod1("setPosition", new Object[] { entity, x, y, z });
 	}
 
 	public void setPositionRelative(Object entity, double x, double y, double z) {
-		MainManager.callMethod("setPositionRelative", new Object[] { entity, x,
-				y, z });
+		mm.callMethod1("setPositionRelative", new Object[] { entity, x, y, z });
 	}
 
 	public void setRot(Object ent, double vertical, double horizontal) {
-		MainManager.callMethod("setRot", new Object[] { ent, vertical,
-				horizontal });
+		mm.callMethod1("setRot", new Object[] { ent, vertical, horizontal });
 	}
 
 	public void setTile(int x, int y, int z, int id, int damage) {
-		MainManager.callMethod("setTile", new Object[] { x, y, z, id, damage });
+		mm.callMethod1("setTile", new Object[] { x, y, z, id, damage });
 	}
 
 	public void setVelX(Object entity, double speed) {
-		MainManager.callMethod("setVelX", new Object[] { entity, speed });
+		mm.callMethod1("setVelX", new Object[] { entity, speed });
 	}
 
 	public void setVelY(Object entity, double speed) {
-		MainManager.callMethod("setVelY", new Object[] { entity, speed });
+		mm.callMethod1("setVelY", new Object[] { entity, speed });
 	}
 
 	public void setVelZ(Object entity, double speed) {
-		MainManager.callMethod("setVelZ", new Object[] { entity, speed });
+		mm.callMethod1("setVelZ", new Object[] { entity, speed });
 	}
 
 	public void spawnChicken(double x, double y, double z, String skinURL) {
-		MainManager.callMethod("spawnChicken",
-				new Object[] { x, y, z, skinURL });
+		mm.callMethod1("spawnChicken", new Object[] { x, y, z, skinURL });
 	}
 
 	public void spawnCow(double x, double y, double z, String skinURL) {
-		MainManager.callMethod("spawnCow", new Object[] { x, y, z, skinURL });
+		mm.callMethod1("spawnCow", new Object[] { x, y, z, skinURL });
 	}
 
 	public void spawnPigZombie(double x, double y, double z, int id,
 			String skinURL) {
-		MainManager.callMethod("spawnPigZombie", new Object[] { x, y, z, id,
-				skinURL });
+		mm.callMethod1("spawnPigZombie", new Object[] { x, y, z, id, skinURL });
+	}
+
+	public class ModPEObject {
+		public void dumpVtable(String s1, String s2) {
+			mm.callMethod1("ModPE.dumpVtable", new Object[] { s1, s2 });
+		}
+
+		public byte[] getBytesFromTexturePack(String address) {
+			return (byte[]) mm.callMethod1("ModPE.getBytesFromTexturePack",
+					new Object[] { address });
+		}
+
+		public String getMinecraftVersion() {
+			return (String) mm.callMethod1("ModPE.getMinecraftVersion",
+					new Object[] {});
+		}
+
+		public void langEdit(String from, String to) {
+			mm.callMethod1("ModPE.langEdit", new Object[] { from, to });
+		}
+
+		public void dumpVtable(String s) {
+			mm.callMethod1("ModPE.dumpVtable", new Object[] { s });
+		}
+
+		public void dumpVtable(String s) {
+			mm.callMethod1("ModPE.dumpVtable", new Object[] { s });
+		}
+
+		public void dumpVtable(String s) {
+			mm.callMethod1("ModPE.dumpVtable", new Object[] { s });
+		}
+
+		public void dumpVtable(String s) {
+			mm.callMethod1("ModPE.dumpVtable", new Object[] { s });
+		}
+
+		public void dumpVtable(String s) {
+			mm.callMethod1("ModPE.dumpVtable", new Object[] { s });
+		}
+
+		public void dumpVtable(String s) {
+			mm.callMethod1("ModPE.dumpVtable", new Object[] { s });
+		}
+
+		public void dumpVtable(String s) {
+			mm.callMethod1("ModPE.dumpVtable", new Object[] { s });
+		}
+
+		public void dumpVtable(String s) {
+			mm.callMethod1("ModPE.dumpVtable", new Object[] { s });
+		}
+
+		public void dumpVtable(String s) {
+			mm.callMethod1("ModPE.dumpVtable", new Object[] { s });
+		}
+
+		public void dumpVtable(String s) {
+			mm.callMethod1("ModPE.dumpVtable", new Object[] { s });
+		}
+
+		public void dumpVtable(String s) {
+			mm.callMethod1("ModPE.dumpVtable", new Object[] { s });
+		}
+
+		public void dumpVtable(String s) {
+			mm.callMethod1("ModPE.dumpVtable", new Object[] { s });
+		}
 	}
 }
