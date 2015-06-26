a29c872cb7928c1171ad6bb42b8b431397a4ef45
nao20010128nao <nao20010128@gmail.com>
2015-06-26 19:11:44 +0900

    Effect ID一覧作成

---
+++ javaSrc/JavaFunctions/src/modules/consistants/EffectID.java
@@ -0,0 +1,39 @@
+package modules.consistants;
+
+public final class EffectID {
+	private EffectID() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public final static int EFFECT_SPEED = 1;
+	public final static int EFFECT_SLOWNESS = 2;
+	public final static int EFFECT_HASTE = 3;
+	public final static int EFFECT_MINING_FATIGUE = 4;
+	public final static int EFFECT_STRENGTH = 5;
+	public final static int EFFECT_INSTANT_HEALTH = 6;
+	public final static int EFFECT_INSTANT_DAMAGE = 7;
+	public final static int EFFECT_JUMP_BOOST = 8;
+	public final static int EFFECT_NAUSEA = 9;
+	public final static int EFFECT_REGENERATION = 10;
+	public final static int EFFECT_RESISTANCE = 11;
+	public final static int EFFECT_FIRE_RESISTANCE = 12;
+	public final static int EFFECT_WATER_BREATHING = 13;
+	public final static int EFFECT_INVISIILITY = 14;
+	public final static int EFFECT_BLINDNESS = 15;
+	public final static int EFFECT_NIGHT_VISION = 16;
+	public final static int EFFECT_HUNGER = 17;
+	public final static int EFFECT_WEAKNESS = 18;
+	public final static int EFFECT_POISON = 19;
+	public final static int EFFECT_WITHER = 20;
+	public final static int EFFECT_HEALTH_BOOST = 21;
+	/**
+	 * @deprecated It is not implemented in MC:PE. But MC:PE accepts this.
+	 * */
+	@Deprecated
+	public final static int EFFECT_ABSORPTION = 22;
+	/**
+	 * @deprecated It is not implemented in MC:PE. But MC:PE accepts this.
+	 * */
+	@Deprecated
+	public final static int EFFECT_SATURATION = 23;
+}
