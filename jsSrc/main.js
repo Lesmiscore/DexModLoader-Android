ff301b31d7e199a7c2e37bf4b15c15e49ad184ca
nao20010128nao <nao20010128@gmail.com>
2015-06-27 14:09:31 +0900

    仕様変更に伴う変更

--- jsSrc/main.js
+++ jsSrc/main.js
@@ -6,7 +6,6 @@
 var io = java.io;
 var lang = java.lang;
 var mainDex = "";
-var dxTool = "";
 var mainManager, dxClass, hookObserver;
 var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
 {
@@ -32,30 +31,6 @@ var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
         .newInstance(fi.toString(), dcodedir.toString(), null, ctx.getClassLoader());
     //The main object in the DEX file.
     mainManager = dxc.loadClass("modules.MainManager").getConstructor(lang.Class.forName("android.content.Context")).newInstance(ctx);
-    //CLASSLOADER AREA END
-}
-{
-    //CLASSLOADER AREA
-    /*Base 64 string of the DEX file*/
-    var array = android.util.Base64.decode(dxTool, 0);
-    var fi = io.File(ctx.getFilesDir(), "javaModLoaderDxTool.dex");
-    var fos;
-    try {
-        fos = io.FileOutputStream(fi);
-        fos.write(array);
-    } finally {
-        if (fos) fos.close();
-    }
-    var dcodedir;
-    if ((android.os.Build.VERSION.SDK_INT - 21) < 0) {
-        dcodedir = ctx.getDir("optimizedDex", 0);/*Older Android(~4.4W)*/
-    } else {
-        dcodedir = ctx.getCodeCacheDir();/*Newer Android(5.0~)*/
-    }
-    var dxc = lang.Class.forName("dalvik.system.DexClassLoader")
-        .getConstructor(strCls, strCls, strCls, lang.Class.forName("java.lang.ClassLoader"))
-        .newInstance(fi.toString(), dcodedir.toString(), null, ctx.getClassLoader());
-    //The main object in the DEX file.
     dxClass = dxc.loadClass("com.android.dx.command.Main");
     //CLASSLOADER AREA END
 }
