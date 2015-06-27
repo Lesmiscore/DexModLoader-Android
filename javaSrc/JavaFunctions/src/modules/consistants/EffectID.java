c22bd6400662c1a156510c5043d1cade33612333
nao20010128nao <nao20010128@gmail.com>
2015-06-27 22:05:34 +0900

    Effect IDのプリフィックス(EFFECT_)を削除

--- javaSrc/JavaFunctions/src/modules/consistants/EffectID.java
+++ javaSrc/JavaFunctions/src/modules/consistants/EffectID.java
@@ -1,40 +1,39 @@
 package modules.consistants;
 
-
 public final class EffectID {
 	private EffectID() {
 		// TODO 自動生成されたコンストラクター・スタブ
 	}
 
-	public final static int EFFECT_SPEED = 1;
-	public final static int EFFECT_SLOWNESS = 2;
-	public final static int EFFECT_HASTE = 3;
-	public final static int EFFECT_MINING_FATIGUE = 4;
-	public final static int EFFECT_STRENGTH = 5;
-	public final static int EFFECT_INSTANT_HEALTH = 6;
-	public final static int EFFECT_INSTANT_DAMAGE = 7;
-	public final static int EFFECT_JUMP_BOOST = 8;
-	public final static int EFFECT_NAUSEA = 9;
-	public final static int EFFECT_REGENERATION = 10;
-	public final static int EFFECT_RESISTANCE = 11;
-	public final static int EFFECT_FIRE_RESISTANCE = 12;
-	public final static int EFFECT_WATER_BREATHING = 13;
-	public final static int EFFECT_INVISIILITY = 14;
-	public final static int EFFECT_BLINDNESS = 15;
-	public final static int EFFECT_NIGHT_VISION = 16;
-	public final static int EFFECT_HUNGER = 17;
-	public final static int EFFECT_WEAKNESS = 18;
-	public final static int EFFECT_POISON = 19;
-	public final static int EFFECT_WITHER = 20;
-	public final static int EFFECT_HEALTH_BOOST = 21;
+	public final static int SPEED = 1;
+	public final static int SLOWNESS = 2;
+	public final static int HASTE = 3;
+	public final static int MINING_FATIGUE = 4;
+	public final static int STRENGTH = 5;
+	public final static int INSTANT_HEALTH = 6;
+	public final static int INSTANT_DAMAGE = 7;
+	public final static int JUMP_BOOST = 8;
+	public final static int NAUSEA = 9;
+	public final static int REGENERATION = 10;
+	public final static int RESISTANCE = 11;
+	public final static int FIRE_RESISTANCE = 12;
+	public final static int WATER_BREATHING = 13;
+	public final static int INVISIILITY = 14;
+	public final static int BLINDNESS = 15;
+	public final static int NIGHT_VISION = 16;
+	public final static int HUNGER = 17;
+	public final static int WEAKNESS = 18;
+	public final static int POISON = 19;
+	public final static int WITHER = 20;
+	public final static int HEALTH_BOOST = 21;
 	/**
 	 * @deprecated It is not implemented in MC:PE. But MC:PE accepts this.
 	 * */
 	@Deprecated
-	public final static int EFFECT_ABSORPTION = 22;
+	public final static int ABSORPTION = 22;
 	/**
 	 * @deprecated It is not implemented in MC:PE. But MC:PE accepts this.
 	 * */
 	@Deprecated
-	public final static int EFFECT_SATURATION = 23;
+	public final static int SATURATION = 23;
 }
