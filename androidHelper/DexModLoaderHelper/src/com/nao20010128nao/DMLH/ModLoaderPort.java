bdba1f22b61f4b6fc7b934110a585bb19e7944b4
nao20010128@gmail.com <nao20010128@gmail.com>
2015-06-28 11:24:29 +0900

    データ受信機作成中

---
+++ androidHelper/DexModLoaderHelper/src/com/nao20010128nao/DMLH/ModLoaderPort.java
@@ -0,0 +1,11 @@
+package com.nao20010128nao.DMLH;
+import android.content.*;
+
+public class ModLoaderPort extends BroadcastReceiver
+{
+	@Override
+	public void onReceive(Context p1, Intent p2)
+	{
+		// TODO: Implement this method
+	}
+}
