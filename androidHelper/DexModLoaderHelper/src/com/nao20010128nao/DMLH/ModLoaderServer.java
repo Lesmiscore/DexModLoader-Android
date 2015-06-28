f21291ef88c1d2ad389fdb8ba0b1c196b8308881
nao20010128@gmail.com <nao20010128@gmail.com>
2015-06-28 09:29:23 +0900

    Server作成

---
+++ androidHelper/DexModLoaderHelper/src/com/nao20010128nao/DMLH/ModLoaderServer.java
@@ -0,0 +1,16 @@
+package com.nao20010128nao.DMLH;
+import android.app.*;
+import android.content.*;
+import android.os.*;
+
+public class ModLoaderServer extends Service
+{
+
+	@Override
+	public IBinder onBind(Intent p1)
+	{
+		// TODO: Implement this method
+		return null;
+	}
+	
+}
