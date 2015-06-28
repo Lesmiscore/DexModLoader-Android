2be370fa3c97472eefd0dd831bb3e2c2f12cf494
nao20010128nao <nao20010128@gmail.com>
2015-06-28 21:51:04 +0900

    途中

--- runme.ps1
+++ runme.ps1
@@ -1,1 +1,15 @@
 ﻿echo "==================="
+echo "Java Mod Loader for BlockLauncher(Android) builder"
+echo "==================="
+echo "Starting build..."
+$webClient=New-Object -TypeName System.Net.WebClient
+echo "Downloading latest js file..."
+$jsFile=$webClient.DownloadString("https://github.com/nao20010128nao/DexModLoader-Android/raw/master/jsSrc/main.js")
+echo "Downloading latest jar file..."
+$webClient.DownloadFile("https://github.com/nao20010128nao/DexModLoader-Android/raw/master/resources/app.jar")
+echo "Converting jar file..."
+[System.Diagnostics.Process]::Start([System.Environment]::GetEnvironmentVariable("ComSpec"),"/c ./dx/dx.bat --dex").WaitForExit()
+$jfB64=[System.Convert]::ToBase64String($jarFile)
+echo "Making js file..."
+$jsResult=$jsFile.Replace("%mainDex%","")
+echo "Downloading latest jar file..."
