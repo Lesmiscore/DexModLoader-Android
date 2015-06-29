2cd2d7c4a7d930aeb6d0f534eb2c75e4cc0d0921
nao20010128nao <nao20010128@gmail.com>
2015-06-29 20:10:00 +0900

    修正

--- runme.ps1
+++ runme.ps1
@@ -7,10 +7,10 @@ echo "Downloading latest js file..."
 $jsFile=$webClient.DownloadString("https://github.com/nao20010128nao/DexModLoader-Android/raw/master/jsSrc/main.js")
 echo "Downloading latest jar file..."
 $webClient.DownloadFile("https://github.com/nao20010128nao/DexModLoader-Android/raw/master/resources/app.jar","./file.jar")
-$jarFile=[system.io.file]::ReadAllBytes("./file.jar")
 echo "Converting jar file..."
 java -classpath ./file.jar modules.build.BuildTask ./file2.jar ./file.jar ./dx/lib/dx.jar
 java -jar ./dx/lib/dx.jar --dex --output=./dex.zip --multi-dex ./file2.jar
+$jarFile=[system.io.file]::ReadAllBytes("./dex.zip")
 $jfB64=[System.Convert]::ToBase64String($jarFile)
 echo "Making js file..."
 $jsResult=$jsFile.Replace("%mainDex%",$jfB64)
