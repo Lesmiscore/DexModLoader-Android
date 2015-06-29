c66addb5e4ee455f669fe61be6702b82e5b32288
nao20010128nao <nao20010128@gmail.com>
2015-06-29 21:45:52 +0900

    コードをフォーマット

--- androidHelper/DexModLoaderHelper/src/com/nao20010128nao/DMLH/ModLoaderPort.java
+++ androidHelper/DexModLoaderHelper/src/com/nao20010128nao/DMLH/ModLoaderPort.java
@@ -1,11 +1,10 @@
 package com.nao20010128nao.DMLH;
+
 import android.content.*;
 
-public class ModLoaderPort extends BroadcastReceiver
-{
+public class ModLoaderPort extends BroadcastReceiver {
 	@Override
-	public void onReceive(Context p1, Intent p2)
-	{
+	public void onReceive(Context p1, Intent p2) {
 		// TODO: Implement this method
 	}
 }
